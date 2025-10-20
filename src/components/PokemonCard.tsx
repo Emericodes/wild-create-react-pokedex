aefe2ca92171fe3ffa9737188fb02379fb81c517
FIRST_NAME LAST_NAME <Pro.emeric.m@gmail.com>
2025-10-21 00:13:57 +0200

    balbusaur attack Razor Leafgit add . commit a component first PokemonCard

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+	return (
+		<figure>
+			<img
+				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+				aria-label="Bulbasaur is a pokemonster, a little monster for videogame"
+			/>
+			<figcaption>Balbusaur</figcaption>
+		</figure>
+	);
+}
+
+export default PokemonCard;
