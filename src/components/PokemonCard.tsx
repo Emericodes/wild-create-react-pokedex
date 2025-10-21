25ee1ecc01c28e06ddb8fb5535a067f5b23e4f26
FIRST_NAME LAST_NAME <Pro.emeric.m@gmail.com>
2025-10-21 17:30:00 +0200

    creation a new branch and modify the f.... PokemonCard.tsx with a f...... ternaire

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,25 @@
-function PokemonCard() {
-	return (
-		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				aria-label="Bulbasaur is a pokemonster, a little monster for videogame"
-			/>
-			<figcaption>Balbusaur</figcaption>
-		</figure>
-	);
-}
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
+function PokemonCard() {
+  const pokemon = pokemonList[1];
+  const pokemonImg = pokemonList[1].imgSrc;
+
+  return (
+    <figure>
+      {pokemonImg != null ? <img src = {pokemonImg} alt={pokemon.name}/> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
 export default PokemonCard;
+
+
